52033e6a9c380b78602887d97caafac96b37175e
yl3218 <yl3218@gmail.com>
2017-01-06 22:50:19 +0800

    fix
    兩個class檔合併

--- src/main.java
+++ src/main.java
@@ -10,3 +10,25 @@ public class main {
 		System.out.println(ReverseWords.reverse(words));
 	}
 }
+
+class ReverseWords {
+
+	static String[] iwords;
+	static String out = "";
+
+	public static String reverse(String[] words) {
+		iwords = words;
+		rev(words.length);
+		return out.toString();
+	}
+
+	public static int rev(int i) {
+		if (i == 0)
+			return 1;
+		else {
+			out += iwords[i - 1] + " ";
+			return rev(i - 1);
+		}
+	}
+
+}
